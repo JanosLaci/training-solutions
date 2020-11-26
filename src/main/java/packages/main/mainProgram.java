f9f9f09d96d52d4b674f763e625c740c808d1c38
Laszlo Janos <janoslaszlo@gmail.com>
2020-11-26 22:04:30 +0100

    commit és push 2020. november 26-án (János László)
    feladat: https://e-learning.training360.com/courses/take/java-se-alapok-java-nyelvi-elemek/lessons/10709796-csomagok
    
    "Köszöntés
     Hozz létre a packages.greetings csomagban egy Greeter osztályt, melynek legyen egy public void sayHello() metódusa, mely kiírja, hogy Hello World!.
    
     Hozz létre a packages.main csomagban egy MainProgram osztályt, melynek legyen egy main() metódusa. Ez példányosítsa a Greeter osztályt, majd hívja meg annak sayHello() metódusát!"

---
+++ src/main/java/packages/main/mainProgram.java
@@ -0,0 +1,12 @@
+package packages.main;
+
+public class mainProgram {
+
+    public static void main(String[] args) {
+
+        packages.greetings.Greeter myGreeter = new packages.greetings.Greeter();
+
+        myGreeter.sayHello();
+
+    }
+}
