b5894fa54d160746899f6144fd03341dc93c4b65
Laszlo Janos <janoslaszlo@gmail.com>
2020-11-26 23:41:24 +0100

    commit és push 2020. november 26-án (János László)
    feladat: https://e-learning.training360.com/courses/take/java-se-alapok-java-nyelvi-elemek/lessons/10709757-java-api
    
    "String osztály
     Keresd ki a JDK API dokumentációból, a String osztálynál, hogyan lehet egy karakterláncot nagybetűssé tenni!
    
     Írj egy Upper osztályt, ami a Hello World! szöveget nagybetűssé alakítja!"

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,13 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!".toUpperCase());
+
+        String toBeConvertedUpperCase = "Hello World!";
+        String convertedToUpperCase = toBeConvertedUpperCase.toUpperCase();
+        System.out.println(convertedToUpperCase);
+
+    }
+}
