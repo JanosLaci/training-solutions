c058ce9884a4854d06077854d4c08213beb4b034
János László <janoslaszlo@gmail.com>
2020-12-18 13:15:46 +0100

    commit és push 2020 december 18-án (János László)
    Feladat:
    
    "Kristóf Bárczay  9:25 AM
    Mai Junior/Mid-level feladat:
    A week08d05 csomagban készíts egy osztályt MathAlgorithms néven. Legyen benne egy metódus, greatestCommonDivisor() , ami paraméterül vár két pozitív egész számot és visszaadja a legnagyobb közös osztójukat."

---
+++ src/main/java/week08d05/MathAlgorithms.java
@@ -0,0 +1,29 @@
+package week08d05;
+
+//Kristóf Bárczay  9:25 AM
+//Mai Junior/Mid-level feladat:
+//A week08d05 csomagban készíts egy osztályt MathAlgorithms néven.
+// Legyen benne egy metódus, greatestCommonDivisor() ,
+// ami paraméterül vár két pozitív egész számot és visszaadja a legnagyobb közös osztójukat.
+
+
+public class MathAlgorithms {
+    int firstInputInt, secondInputInt;
+
+    public int greatestCommonDivisor(int firstInputInt, int secondInputInt){
+        int foundMaxDivisor = 1;
+        for (int possibleDivisor = 1; possibleDivisor <= Math.min(firstInputInt, secondInputInt); possibleDivisor++){
+
+            if (firstInputInt % possibleDivisor == 0 && secondInputInt % possibleDivisor == 0){
+                foundMaxDivisor = possibleDivisor;
+            }
+
+
+        }
+        return foundMaxDivisor;
+
+    }
+
+
+
+}
---
+++ src/main/java/week08d05/MathAlgorithmsMain.java
@@ -0,0 +1,29 @@
+package week08d05;
+
+import java.util.Scanner;
+
+public class MathAlgorithmsMain {
+    public static void main(String[] args) {
+
+        Scanner scannerForMathAlgorithms = new Scanner(System.in);
+        System.out.println("Számítsuk ki két pozitív egész szám legnagyobb közös osztóját!");
+        System.out.println("Első szám?");
+        int myFirstInputInt = scannerForMathAlgorithms.nextInt();
+        String enterHandler = scannerForMathAlgorithms.nextLine();
+
+        System.out.println("Második szám?");
+
+        int mySecondInputInt = scannerForMathAlgorithms.nextInt();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
+
+        int myFoundGreatestCommonDivisor = mathAlgorithms.greatestCommonDivisor(myFirstInputInt, mySecondInputInt);
+
+        System.out.println(myFirstInputInt + " " + mySecondInputInt + " legnagyobb közös osztója " + myFoundGreatestCommonDivisor);
+
+
+
+
+    }
+
+
+}
