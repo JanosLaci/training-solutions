3c74ceabbedbf9b3c8b30a37860b139f8281d5ad
János László <janoslaszlo@gmail.com>
2021-01-22 11:41:59 +0100

    commit és push 2021. január 22-én (János László)
    //Viczián István  8:53 AM Mai junior/mid feladat:
    //Töltsd le az index.hu nyitólapját böngészőből, ez egy html állomány!
    // Másold be a projektedbe (index.html) néven! Ez egy szöveges állomány.
    // Olvasd be, és számold meg, hány sorban szerepel a koronavírus szó!
    
    //Megj.: a html file útvonala: src/main/resources/indexHuInHtmlFormat/Index.html

---
+++ src/main/java/week12d05/WordContainingLinesCounterFromHtml.java
@@ -0,0 +1,35 @@
+package week12d05;
+
+//Viczián István  8:53 AM Mai junior/mid feladat:
+//Töltsd le az index.hu nyitólapját böngészőből, ez egy html állomány!
+// Másold be a projektedbe (index.html) néven! Ez egy szöveges állomány.
+// Olvasd be, és számold meg, hány sorban szerepel a koronavírus szó!
+//Megj.: a html file útvonala: src/main/resources/indexHuInHtmlFormat/Index.html
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class WordContainingLinesCounterFromHtml {
+    public static void main(String[] args) throws IOException {
+
+        String fileLocation = "src/main/resources/indexHuInHtmlFormat/Index.html";
+        Path htmlPath = Path.of(fileLocation);
+
+        int wordCounter = 0;
+        int wordCounterCaseInsensitive = 0;
+
+        List<String> rowsFromHtml = Files.readAllLines(htmlPath);
+
+        for (String rowString : rowsFromHtml) {
+            if (rowString.contains("koronavírus")) wordCounter++;
+            if (rowString.contains("koronavírus") || rowString.contains("Koronavírus")) wordCounterCaseInsensitive++;
+        }
+
+        System.out.println("A \"koronavírus\" szó "
+                + wordCounter + " sorban jelenik meg legalább egyszer az \"" + fileLocation + "\" állományban.");
+        System.out.println("A \"koronavírus\" vagy a \"Koronavírus\" szó "
+                + wordCounterCaseInsensitive + " sorban jelenik meg legalább egyszer az \"" + fileLocation + "\" állományban.");
+    }
+}
