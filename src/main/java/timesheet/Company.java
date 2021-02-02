a8a45906d9ea16a3810a42ee2cac051fdff177d6
János László <janoslaszlo@gmail.com>
2021-02-02 12:50:38 +0100

    commit és push 2021. február 2-án (János László)
    clean-up push & commit

--- src/main/java/timesheet/Company.java
+++ src/main/java/timesheet/Company.java
@@ -1,3 +1,4 @@
+/*
 package timesheet;
 
 //A Company osztály tartalmazza
@@ -19,11 +20,11 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Company {
-
-
 
 
     private InputStream
@@ -31,11 +32,14 @@ public class Company {
 
     private List<Employee> employees;
     private List<Project> projects;
+    private List<TimeSheetItem> timeSheetItems = new ArrayList<>();
 
     public Company(InputStream employeesFile, InputStream projectsFile) {
 
         this.employeesFile = employeesFile;
         this.projectsFile = projectsFile;
+
+        //kérdés: itt érdemes példányosítani, vagy az attribútumnál?
 
 
         List<Employee> generatedListOfEmployees = new ArrayList<>();
@@ -47,15 +51,14 @@ public class Company {
         String projectNameFromFile;
         String employeeNameFromFile;
         try {
-        while((projectNameFromFile=readerOfProjects.readLine())!=null){
-            generatedListOfProjects.add(new Project(projectNameFromFile));
-        }
-        while((employeeNameFromFile=readerOfEmployees.readLine())!=null){
-            generatedListOfEmployees.add(new Employee(employeeNameFromFile));
-        }
-        }
-        catch (IOException e) {
-            throw  new IllegalStateException("Cannot read file.", e);
+            while ((projectNameFromFile = readerOfProjects.readLine()) != null) {
+                generatedListOfProjects.add(new Project(projectNameFromFile));
+            }
+            while ((employeeNameFromFile = readerOfEmployees.readLine()) != null) {
+                generatedListOfEmployees.add(new Employee(employeeNameFromFile));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read file.", e);
         }
 
         this.employees = generatedListOfEmployees;
@@ -64,19 +67,38 @@ public class Company {
     }
 
 
+    public void addTimeSheetItem(Employee employee, Project project, LocalDateTime beginDate, LocalDateTime endDate) {
+        timeSheetItems.add(new TimeSheetItem(employee, project, beginDate, endDate));
 
-    public TimeSheetItem addTimeSheetItem
-            (Employee employee, Project project, LocalDateTime beginDate, LocalDateTime endDate) {
 
-
-        return new TimeSheetItem(employee, project, beginDate, endDate);
     }
 
     //A calculateProjectByNameYearMonth() metódus adja vissza,
 // hogy az adott alkalmazott az adott évben és hónapban melyik projekten mennyit dolgozott.
 
-    public List<ReportLine> calculateProjectByNameYearMonth(String employeeName, int year, int month){
+   public List<ReportLine> calculateProjectByNameYearMonth(String employeeName, int year, int month){
         List<ReportLine> outPutListProjectByNameYearMonth = new ArrayList<>();
+
+        //Map előállítása a Company osztályban lévő TimesheetItem-ek listájából,
+       // amit az addTimeSheetItem metódus állított össze
+       //ez a employeeNameToListOfTimeSheetItemsMap minden employee.getName() String kulcshoz
+       //(mivel a kalkulációhoz az employee nevét kérjük - ez egyben feltételezi, h nincs ugyanolyan nevű alkalmazott)
+       // visszaadja az employeehoz tartozó eddig felvitt TimesheetItem-eket egy listában
+       //kérdés: nem lenne-e jobb egy halmaz, akkor úgymond a véletlen, kétszer bevitt duplikációkat kiszűrné
+
+        Map<String, List<TimeSheetItem>> employeeNameToListOfTimeSheetItemsMap = new HashMap<>();
+        List<TimeSheetItem> perEmployeeTimesheetItemsList = new ArrayList<>();
+
+        for (TimeSheetItem timeSheetItem:
+             timeSheetItems) {
+
+            if (!employeeNameToListOfTimeSheetItemsMap.containsKey(timeSheetItem.getEmployee().getName())) {
+                employeeNameToListOfTimeSheetItemsMap.put(timeSheetItem.getEmployee().getName(), )
+            }
+
+
+        }
+
 
 
         return outPutListProjectByNameYearMonth;
@@ -85,11 +107,10 @@ public class Company {
 
 
 
-
     public void printToFile(String employeeName, int year, int month, Path file) {
 
         try {
-            Writer writer  = Files.newBufferedWriter(file, StandardOpenOption.APPEND);
+            Writer writer = Files.newBufferedWriter(file, StandardOpenOption.APPEND);
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read the file.", ioe);
         }
@@ -116,3 +137,4 @@ public class Company {
 
 
 }
+*/
